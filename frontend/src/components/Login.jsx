2ffdf2a40fe83681efef419fb956f343119503fe
HagaiLevHacohen <levhacohen@mail.tau.ac.il>
2025-12-29 15:58:01 +0200

    fixex error message in login

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -34,7 +34,7 @@ export default function Login() {
       const data = await res.json();
 
       if (!res.ok) {
-        throw new Error(data.message || "Login failed");
+        throw new Error(data.error || "Login failed");
       }
 
       // backend should return { token }
